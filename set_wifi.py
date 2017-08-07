ba86e8cad45f3849fb26638d903d64987bb0d44e
Luke Sweeney <luke@thesweeneys.org>
2017-08-06 22:12:56 -0500

    Adds backed form handler

--- set_wifi.py
+++ set_wifi.py
@@ -1,9 +1,18 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 
-def home(name=None):
-    # Return a view
-    return render_template("set_wifi.html", name=name)
+def index():
+    return render_template("set_wifi.html")
+
+@app.route("/set-wifi", methods = ["POST", "GET"])
+
+def handle_form_post():
+    if request.method == "POST":
+        return write_wpa_supplicant(request.form['ssid'], request.form["password"])
+    else:
+        return "Bad request"
+
+def write_wpa_supplicant(ssid, password):
+    return ssid
