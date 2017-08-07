00125e05ee8fee2b8c9391338345becbae4cc2e5
Luke Sweeney <luke@thesweeneys.org>
2017-08-06 23:54:46 -0500

    Extracts validation to method

--- set_wifi.py
+++ set_wifi.py
@@ -11,13 +11,16 @@ def index():
 @app.route("/set-wifi", methods = ["POST", "GET"])
 
 def handle_form_post():
-    for field, value in request.form.iteritems():
+    validate(request.form)
+    write_wpa_supplicant(request.form['ssid'], request.form["password"], request.form['priority'])
+    session["message"] = "Successful. Restart your Pi to hae "
+    return redirect("/")
+
+def validate(form):
+    for field, value in form.iteritems():
         if not value:
             session["message"] = "Please fill out the form below"
             return redirect("/")
-    write_wpa_supplicant(request.form['ssid'], request.form["password"], request.form['priority'])
-    session["message"] = "Successful"
-    return redirect("/")
 
 def write_wpa_supplicant(ssid, password, priority):
     # Write to wpa_supplicant
