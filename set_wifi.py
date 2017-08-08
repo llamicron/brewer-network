21632293952ec4c5ba01a6cb151c2fdac6209e96
Luke Sweeney <luke@thesweeneys.org>
2017-08-08 00:24:04 -0500

    Fixes the damn file stream

--- set_wifi.py
+++ set_wifi.py
@@ -8,7 +8,11 @@ wpa_supplicant = "/Users/llamicron/test"
 @app.route("/")
 
 def index():
-    return render_template("set_wifi.html", file = wpa_supplicant)
+    return render_template(
+        "set_wifi.html",
+        file=wpa_supplicant,
+        file_lines=get_file_contents(wpa_supplicant)
+    )
 
 @app.route("/set-wifi", methods = ["POST", "GET"])
 
@@ -25,6 +29,11 @@ def write_wpa_supplicant(ssid, password, priority):
     # Write to wpa_supplicant
     wpa = open(wpa_supplicant, "a")
     wpa.write("\nnetwork={\n\tssid=\"%s\"\n\tpsk=\"%s\"\n\tpriority=%s\n}" % (ssid, password, priority))
+
+
+def get_file_contents(filename):
+    with open(filename, 'r') as content_file:
+        return list(filter(None, content_file.read().splitlines()))
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000)
